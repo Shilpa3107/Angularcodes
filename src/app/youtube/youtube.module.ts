87dc290d2be5cc8435e8d94b6951be1616c247bc
Shilpa <shilpa.sinha3107@gmail.com>
2023-06-23 21:02:42 +0530

    importing the compnent

--- src/app/youtube/youtube.module.ts
+++ src/app/youtube/youtube.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { UserloginComponent } from './userlogin/userlogin.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    UserloginComponent
+  ],
   imports: [
     CommonModule
   ]
