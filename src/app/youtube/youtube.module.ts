3bb5872a7730af304343813075948931e5e7278c
Shilpa <shilpa.sinha3107@gmail.com>
2023-06-23 21:09:48 +0530

    imported the new component

--- src/app/youtube/youtube.module.ts
+++ src/app/youtube/youtube.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { UserloginComponent } from './userlogin/userlogin.component';
+import { HomepageComponent } from './homepage/homepage.component';
 
 
 
 @NgModule({
   declarations: [
-    UserloginComponent
+    UserloginComponent,
+    HomepageComponent
   ],
   imports: [
     CommonModule
