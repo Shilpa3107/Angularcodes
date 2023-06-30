7d7db581c742d7cf24a73f60f508b54eb79f0467
Shilpa <shilpa.sinha3107@gmail.com>
2023-06-30 18:13:52 +0530

    imported the service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { YoutubeModule } from './youtube/youtube.module';
+import { GlserviceService } from './glservice.service';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,7 @@ import { YoutubeModule } from './youtube/youtube.module';
     AppRoutingModule,
     YoutubeModule
   ],
-  providers: [],
+  providers: [GlserviceService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
